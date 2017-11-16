0c0bb52d5e7d04195f9bc571e4b4f8dc6b89458c
russellpierce <russell.s.pierce@gmail.com>
2017-11-16 14:43:17 +0000

    Improve log_if_verbose

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,9 @@
 #' @param ... Args passed to message
 #'
 #' @return None, side-effect only
+#' @importFrom methods getPackageName
 log_if_verbose <- function(...) {
   if (isTRUE(getOption("parquetr.verbose"))) {
-    message("parquetr: ", ...)
+    message(getPackageName(), " in ", sys.calls()[sys.nframe()-1][[1]][1], "(): ", ...)
   }
 }
